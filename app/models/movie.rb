8d2ba3818a4953a16267c324b835d7f4b5f51e39
elenadm <elena.an.dmitrieva@gmail.com>
2014-06-05 22:24:36 +0300

    Fix bug for radio_button.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
   validates :rating, presence: true,
              inclusion: {in: ['G','PG','PG-13','R', 'NC-17'], allow_blank: true }
   validates :release_date, presence: { message: "looks bad"}
-  def self.all_avaliable_ratingsgit
+  def self.all_available_ratings
     ['G','PG','PG-13','R', 'NC-17']
   end
   def self.all_ratings
