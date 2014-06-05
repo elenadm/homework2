1188a2b9ac929e483e3bbaa0dfad706d6f747428
elenadm <elena.an.dmitrieva@gmail.com>
2014-06-05 20:18:37 +0300

    Layout is not finish for radio_button.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,9 @@ class Movie < ActiveRecord::Base
   validates :rating, presence: true,
              inclusion: {in: ['G','PG','PG-13','R', 'NC-17'], allow_blank: true }
   validates :release_date, presence: { message: "looks bad"}
-
+  def self.all_avaliable_ratingsgit
+    ['G','PG','PG-13','R', 'NC-17']
+  end
   def self.all_ratings
     Movie.select('rating').distinct.order(:rating).pluck(:rating)
   end
