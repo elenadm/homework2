6280681a6e6284c06eb216ec1cbe5e6ea70abf09
elenadm <elena.an.dmitrieva@gmail.com>
2014-06-09 17:10:15 +0300

    Implementation of tests for order in scope.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -28,12 +28,34 @@ describe Movie do
         Movie.create(title: 'aaa', rating: 'PG', release_date: Date.today)
         expect(Movie.all_ratings).to eq(['PG'])
       end
-
     end
+
     context ".all_available_ratings" do
       it 'Rating for list - ratings' do
-        expect(Movie.all_available_ratings).to eq(['G','PG','PG-13','R', 'NC-17'])
+        expect(Movie.all_available_ratings).to eq(['G', 'PG', 'PG-13', 'R', 'NC-17'])
       end
     end
+
+    context ":list" do
+      let!(:movie_one) { Movie.create(title: "Pulp fiction", rating: "NC-17", release_date: Date.today) }
+      let!(:movie_two) { Movie.create(title: "The blind side", rating: "PG-13", release_date: 1.day.ago) }
+
+      it 'orders by ascending title' do
+        Movie.all.should eq [movie_one, movie_two]
+      end
+
+      it 'orders by ascending release_date' do
+        Movie.all.order(:release_date).should eq [movie_two, movie_one]
+      end
+
+      it 'orders by descending title' do
+        Movie.all.order(title: :desc).should eq [movie_two, movie_one]
+      end
+
+      it 'orders by descending release_date' do
+        Movie.all.order(release_date: :desc).should eq [movie_one, movie_two]
+      end
+
+    end
   end
-  end
+end
