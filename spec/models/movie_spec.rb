719d9aa55863b3d47e5a2b136291260032871b90
elenadm <elena.an.dmitrieva@gmail.com>
2014-06-09 18:17:09 +0300

    Implementation of test for where in scope.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -37,24 +37,39 @@ describe Movie do
     end
 
     context ":list" do
-      let!(:movie_two) { Movie.create(title: "The blind side", rating: "PG-13", release_date: 1.day.ago) }
       let!(:movie_one) { Movie.create(title: "Pulp fiction", rating: "NC-17", release_date: Date.today) }
+      let!(:movie_two) { Movie.create(title: "The blind side", rating: "PG-13", release_date: 1.day.ago) }
 
-
-      it 'orders by ascending title' do
-        Movie.list(order:("title ASC")).should eq [movie_one, movie_two]
+      it 'Orders by ascending title' do
+        Movie.list(order: ("title ASC")).should eq [movie_one, movie_two]
       end
 
-      it 'orders by ascending release_date' do
-        Movie.list(order:("release_date ASC")).should eq [movie_two, movie_one]
+      it 'Orders by ascending release_date' do
+        Movie.list(order: ("release_date ASC")).should eq [movie_two, movie_one]
       end
 
-      it 'orders by descending title' do
-        Movie.list(order:("title DESC")).should eq [movie_two, movie_one]
+      it 'Orders by descending title' do
+        Movie.list(order: ("title DESC")).should eq [movie_two, movie_one]
       end
 
-      it 'orders by descending release_date' do
-        Movie.list(order:("release_date DESC")).should eq [movie_one, movie_two]
+      it 'Orders by descending release_date' do
+        Movie.list(order: ("release_date DESC")).should eq [movie_one, movie_two]
+      end
+
+      it 'No ratings - no movies' do
+        Movie.list(rating: ['R']).should eq []
+      end
+
+      it 'One rating' do
+        Movie.list(rating: ['NC-17']).should eq [movie_one]
+      end
+
+      it 'Two ratings of list' do
+        Movie.list(rating: ['G', 'PG', 'PG-13', 'R', 'NC-17']).should eq [movie_one, movie_two]
+      end
+
+      it 'Orders with ratings' do
+        Movie.list(rating: ['G', 'PG', 'PG-13', 'R', 'NC-17'], order: ("title DESC")).should eq [movie_two, movie_one]
       end
 
     end
