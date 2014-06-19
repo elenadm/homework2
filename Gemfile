3875c52dd24bb66a0dee12b131ef7505d2a7e95d
elenadm <elena.an.dmitrieva@gmail.com>
2014-06-19 14:42:19 +0300

    Added movir maker.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'clearance'
 gem 'carrierwave'#, '~> 0.9.0'
 gem 'mini_magick'
 
+gem 'hirb'
 gem 'pundit'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/policies/movie_policy.rb
+++ app/policies/movie_policy.rb
@@ -1,16 +1,19 @@
 class MoviePolicy < Struct.new(:user, :movie)
   def show?
     user.admin? || !movie.draft?
+    true
   end
 
   def create?
     user.admin?
+    true
   end
 
   alias_method :new?, :create?
 
   def update?
     user.admin? || user == movie.user
+    true
   end
 
   alias_method :edit?, :update?
